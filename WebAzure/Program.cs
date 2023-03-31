6f17addf2217845424fa0ec00bc336249fb89883
Iryna1122 <Oliinykirina1122@gmail.com>
2023-03-31 19:24:16 +0300

    dds

--- WebAzure/Program.cs
+++ WebAzure/Program.cs
@@ -6,15 +6,15 @@ using Microsoft.Extensions.Logging;
 var builder=new HostBuilder();
 builder.ConfigureWebJobs(p => { p.AddAzureStorageCoreServices(); p.AddAzureStorage(); });
 
-builder.ConfigureLogging((q, a) => {
+//builder.ConfigureLogging((q, a) => {
 
-    a.AddConsole();
-    // string key = q.Configuration["AzureKey"];
-    string key = q.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+//    a.AddConsole();
+//    // string key = q.Configuration["AzureKey"];
+//    string key = q.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
 
-    a.AddApplicationInsightsWebJobs(p => p.InstrumentationKey = key);
+//    a.AddApplicationInsightsWebJobs(p => p.InstrumentationKey = key);
 
-});
+//});
 
 var host = builder.Build();
 
